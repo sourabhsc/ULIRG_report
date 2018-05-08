834ca5a76f0fdd84046ac862da2c986b1fe48af7
sourabh <chauhans.sourabh@gmail.com>
2018-05-08 17:52:36 -0500

    Full code

---
+++ ULIRG_build.py
@@ -0,0 +1,9 @@
+import os
+import galaxy_sky_dark_sub_v4
+import galaxy_xreg_rotate
+import galaxy_header_rename
+#import galaxy_photometry_FLT
+galaxy_sky_dark_sub_v4.main()
+galaxy_xreg_rotate.main()
+galaxy_header_rename.main()
+#galaxy_photometry_FLT.main()
