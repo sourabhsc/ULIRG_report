236599bd93526e378e0a4ba7c2142b55b74ab197
sourabh <sourabh@astro.umn.edu>
2018-09-09 15:31:54 -0500

    final ULIRG build file for UV images

--- ULIRG_build.py
+++ ULIRG_build.py
@@ -2,8 +2,17 @@ import os
 import galaxy_sky_dark_sub_v4
 import galaxy_xreg_rotate
 import galaxy_header_rename
-#import galaxy_photometry_FLT
+import galaxy_photometry_FLT
+import galaxy_astrodrizzle
+import galaxy_phot_scale
+import galaxy_UV_sub
+import galaxy_photometry_LYA
+
 galaxy_sky_dark_sub_v4.main()
 galaxy_xreg_rotate.main()
 galaxy_header_rename.main()
 #galaxy_photometry_FLT.main()
+#galaxy_astrodrizzle.main()
+#galaxy_phot_scale.main()
+#galaxy_photometry_LYA.main()
+#galaxy_UV_sub.main()
